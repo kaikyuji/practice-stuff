3d7af4437281db5dfe69f0e4dee1fe6e3b047d6e
Yuji Kohatsu <111162170+kaikyuji@users.noreply.github.com>
2022-10-31 13:03:37 -0300

    Update script.js

--- counting machine/script.js
+++ counting machine/script.js
@@ -5,7 +5,7 @@ function clicar(){
     let start = Number(document.querySelector('input#start').value)
     let end = Number(document.querySelector('input#end').value)
     let skp = Number(document.querySelector('input#skp').value)
-    if(start == '' || end == '' ||  skp == ''){
+    if(start == '' || end == '' ||  skp == '' || skp < 0){
         text.innerHTML = 'INVALID VALUES'
     }else if(end < start){// CASO O END SEJA MENOR QUE O START, TEREMOS QUE SUBTRAIR O START COM SKIP PRA CHEGAR NO END
         text.innerHTML = '<p>Counting...</p>'
